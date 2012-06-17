6fcb1d603d144edd23ac29b53823ee2c297c44ca
S0rcerer <sashokbad@gmail.com>
2012-06-18 01:05:40 +0400

    Adding name to Tonniks Block

--- StrategicCraft.java
+++ StrategicCraft.java
@@ -12,6 +12,8 @@ public class StrategicCraft{
 		BlockStep b = new BlockStep(254,false);
 		ModLoader.registerBlock(b);
 		ModLoader.addRecipe(new ItemStack(b), new Object[]{"D",Character.valueOf('D'),Block.dirt});
+		b.setBlockName("Tonnik block");
+		ModLoader.addName(b, "Tonniks Block");
 	}
 
 }
